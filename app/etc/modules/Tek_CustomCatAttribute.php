e08715e4348c2bd959db9e941ece34a2874473d8
Jessica <jessicacosta.dev@hotmail.com>
2018-01-15 10:55:03 -0200

    First Commit

---
+++ app/etc/modules/Tek_CustomCatAttribute.php
@@ -0,0 +1,9 @@
+<?xml version="1.0"?>
+<config>
+    <modules>
+        <Tek_CustomCatAttribute>
+            <active>true</active>
+            <codePool>community</codePool>
+        </Tek_CustomCatAttribute>
+    </modules>
+</config>
